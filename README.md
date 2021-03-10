cc533cef05ca93b84da4f9d171ca0310ab148ed6
sinholee <dltlsgh5@gmail.com>
2021-03-10 14:17:50 +0900

    this

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # gitflow_practice
+
+노션(https://www.notion.so/TypeScript-Template-1ea671bb9c2e43e8bfebf3cdf5594ca6)
---
+++ this.js
@@ -0,0 +1,8 @@
+const a = 1;
+
+console.log(this.a);
+console.log(global.a);
+console.log(globalThis.a);
+console.log(this);
+console.log(global);
+console.log(this === global);
